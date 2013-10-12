0d2ba25ad9bfe8561511dd363ce1460c131cd722
william <william@naturalpad.org>
2013-10-12 17:41:14 +0200

    player

---
+++ OctoberChallenge4/Assets/scenes/script/player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+    private float speed;
+    private bool touchRight = false;
+	// Use this for initialization
+	void Start () {
+	    speed=3.0F;
+        //rigidbody.
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        //transform.Translate(x, y, 0);
+        bool errortouch = (Input.GetKey("up") &&  Input.GetKey("right")) || (Input.GetKey("up") &&  Input.GetKey("left")) || (Input.GetKey("down") &&  Input.GetKey("left")) || (Input.GetKey("down") &&  Input.GetKey("right"));   
+        
+        if(!errortouch){
+            if (Input.GetKey("down") || Input.GetKey("up"))
+            {
+                transform.Translate(0, y, 0);
+            }
+            if (Input.GetKey("left") || Input.GetKey("right"))
+            {
+                transform.Translate(x, 0, 0);
+            }
+        }
+    }
+
+    
+}
