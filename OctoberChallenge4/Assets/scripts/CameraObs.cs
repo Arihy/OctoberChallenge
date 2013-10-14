e98fac3463e059ea54fa55e0ed72574f9a2caed9
Arihy <ynallem@gmail.com>
2013-10-14 16:19:56 +0200

    added menu

--- OctoberChallenge4/Assets/scripts/CameraObs.cs
+++ OctoberChallenge4/Assets/scripts/CameraObs.cs
@@ -19,7 +19,10 @@ public class CameraObs : MonoBehaviour, IMessageListener {
 
 	// Update is called once per frame
 	void Update () {
-
+		if(Input.GetKey(KeyCode.Escape))
+			Application.LoadLevel("menu");
+		if(Input.GetKey(KeyCode.R))
+			Application.LoadLevel("scene1");
 	}
 
 	public void OnMessage (eMessageID _messageID, GameObject _sender)
---
+++ OctoberChallenge4/Assets/scripts/menu/TryAgainButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TryAgainButton : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Application.LoadLevel("scene1");
+	}
+
+	void OnGUI()
+	{
+	}
+}
